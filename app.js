6a2cfb7c051b576a0ed08db1073bfc534053a291
Abderrahmane01Rhiabi <rhiabi.abderrahmane@gmail.com>
2023-02-22 22:18:13 +0100

    todolist v1

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var items = ["Buy Food","Cook Food","Ear Food"]; //scoop
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.get("/",(req,res)=>{
+
+var today = new Date();
+
+var options = { 
+    weekday : "long",
+    day : "numeric",
+    month : "long"
+}
+
+const currentDay = today.toLocaleDateString("fr-FR", options);
+
+
+    res.render("list", {
+        todayText : currentDay,
+        newListItems : items
+    })
+
+})
+
+app.post("/",(req,res) =>{
+    const item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+
+})
+
+app.listen(3000, ()=>{
+    console.log("server running on port 3000");
+})
