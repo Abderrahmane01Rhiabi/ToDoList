153ab42cdee3ff5f998067e04d6c10b38767e617
Abderrahmane01Rhiabi <rhiabi.abderrahmane@gmail.com>
2023-03-03 00:50:25 +0100

    create custom list and add new items to it

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ const item3 = new Item({ name : "<- Hit this to delete an item"});
 
 const defaultItems = [item1, item2, item3]
 
+const listsSchema = {
+    name : String,
+    items : [itemsSchema]
+}
+
+const List = mongoose.model("List", listsSchema);
 
 app.get("/",(req,res)=>{
 
@@ -36,8 +42,8 @@ app.get("/",(req,res)=>{
             //add default items
             Item.insertMany(defaultItems).then(()=>{
                 console.log("default items added succesfuly");
-            }).catch((error) =>{
-                console.log(error);
+            }).catch((err) =>{
+                console.log(err);
             })
             res.redirect("/");
         }else{
@@ -58,14 +64,94 @@ app.get("/",(req,res)=>{
 
 })
 
+app.get("/:customListName", (req,res) =>{
+    const customListName = req.params.customListName;
+
+    List.findOne({name : customListName}).then((docs)=>{
+
+        if(docs){
+            console.log(customListName+" list already existe");
+
+            console.log(docs);
+
+             res.render("list", {
+                 currentTitle : docs.name,
+                 newListItems : docs.items
+             } )
+
+        }else{
+            console.log(customListName+" default items added succesfuly");
+
+            const list = new List({
+                name : customListName,
+                items : defaultItems
+            })
+
+            /*In this code, you are creating a new document and 
+            saving it to the database using list.save(). However, you are not waiting for the list.save() method to complete 
+            before redirecting the user to the newly created list using res.redirect(). 
+            This can result in the user being redirected to the list page before the new document is saved to the database.
+
+            To fix this issue, you should use the list.save().then() syntax to wait 
+            for the list.save() method to complete before redirecting the user. Here's the updated code:
+            
+
+            
+            This code creates a new document and saves it to the database using list.save(). 
+            Then, it waits for the list.save() method to complete using .then(). If the list.save() 
+            method completes successfully, the code logs 
+            a success message to the console and redirects the user to the newly created list using res.redirect(). 
+            If there is an error during the list.save() method, the error is logged to the console using .catch().*/
+           
+            list.save().then((result) =>{
+                //console.log("----1----");
+                //console.log(result);
+                //console.log("----2----");
+
+                res.redirect("/"+customListName)
+            }).catch((err) =>{
+                console.log(err);
+            });
+
+        }        
+        
+    }).catch((err) =>{
+        console.log(err);
+    })
+})
+
+
 app.post("/",(req,res) =>{
+
+    const listName = req.body.list;
+
     const itemName = req.body.newItem;
+    console.log("listName : "+listName);
+    const newItem = new Item({
+        name : itemName
+    })
 
-    const itemDoc = new Item({name : itemName})
+    //Take the firts part of date 
+    let currentDay = date.getDate();
+    const day_1 = currentDay.split(" ");
 
-    itemDoc.save()
+    //check if the first part of my date equal to the listName
+    if(listName === day_1[0]){
+        newItem.save();
+        res.redirect("/");
+    }else{
 
-    res.redirect("/");
+        /*To add a new item to the items array and update the document in the database, 
+        you can use the $push operator in your update query */
+        List.updateOne({ name: listName }, { $push: { items: newItem } }).then(() => {
+            console.log("updated succesfuly");
+            res.redirect("/"+listName);
+        }).catch((err) =>{
+            console.log(err);
+        })
+    }
+    
+
 
 })
 
@@ -83,17 +169,11 @@ app.post("/delete", (req,res) =>{
 
 })
 
-app.get("/work",(req,res) =>{
-
-    res.render("list", {
-        currentTitle : "Work list",
-        newListItems : workItems
-    })
-})
 
 app.get("/about", (req,res) =>{
     res.render("about")
 })
+
 
 app.listen(3000, ()=>{
     console.log("server running on port 3000");
